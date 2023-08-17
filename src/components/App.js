e00c0e10bc4051f960e4a7afa33d525d40625be6
+ <+>
2023-08-16 23:24:44 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+
 
 function App() {
+
+  const username = "Liza";
+  const city = "New York";
+
+  const image = "https://i.imgur.com/mV8PQxj.gif";
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username ={username} city={city}/>
+      <About image={image}/>
     </div>
   );
 }
